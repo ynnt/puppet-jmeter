616cdd1a80f65464772e838b9e8eb7a6906c40c7
dduvnjak <dario@atlantbh.com>
2013-05-14 14:51:24 +0200

    Fixed conditional plugin installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,7 +39,7 @@ class jmeter(
     require => Exec['download-jmeter'],
   }
 
-  if $jmeter_plugins_install {  
+  if $jmeter_plugins_install == True {  
     exec { 'download-jmeter-plugins':
       command => "wget -P /root http://jmeter-plugins.googlecode.com/files/JMeterPlugins-${jmeter_plugins_version}.zip",
       creates => '/root/JMeterPlugins-${jmeter_plugins_version}.zip'
